6d5f14d760e099d6411f1c7dbb64731add12f339
Fedor <kurnosenkovf@mail.ru>
2022-12-11 17:45:27 +0400

    fuction_1

--- test_finaly/Program.cs
+++ test_finaly/Program.cs
@@ -14,3 +14,32 @@ Console.WriteLine($"{PrintArray(ArrayStrings)}");
 Console.WriteLine();
 
 int length = 3; 
+
+int CheckArray(string[] array, int length)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= length) result++;
+    }
+    return result;
+}
+
+int numbers = CheckArray(ArrayStrings, length); 
+
+string[] newArrayStrings = new string[numbers]; 
+NewArray(ArrayStrings, newArrayStrings, length);
+
+
+void NewArray(string[] oldArray, string[] newArray, int lengthLimit)
+{
+    int temp = 0;
+    for (int i = 0; i < oldArray.Length; i++)
+    {
+        if (oldArray[i].Length <= lengthLimit)
+        {
+            newArray[temp] = oldArray[i];
+            temp++;
+        }
+    }
+}
