34d1f799736dbe1854688278fd8194ca88eb4c9d
Fedor <kurnosenkovf@mail.ru>
2022-12-11 17:31:16 +0400

    start_work

---
+++ test_finaly/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+string[] ArrayStrings = new string[] { };
+Console.Write("Введите количество элементов массива - строк:");
+int n = int.Parse(Console.ReadLine());
+ArrayStrings = new string[n];
+for (int i = 0; i < ArrayStrings.Length; i++)
+{
+    Console.Write($" Введите {i + 1} строку: ");
+    ArrayStrings[i] = Console.ReadLine();
+}
+Console.WriteLine();
+Console.WriteLine("массив введенных данных:");
+Console.WriteLine($"{PrintArray(ArrayStrings)}");
+Console.WriteLine();
+
+int length = 3; 
